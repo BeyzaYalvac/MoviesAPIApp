38d0a874ce3f54361ae67aeeac89123e20dbc773
beyza.yalvac <beyza.yalvac03@gmail.com>
2024-08-25 01:26:53 +0300

    Firebase entegresi ve favori filmler için local database eklendi. Favori listeleme sayfası eklendi. Slider eklendi.

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ await Firebase.initializeApp(
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
+//
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
